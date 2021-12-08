68e7ed449a03eb24c3c38de1ce94e59085ef4604
Maicol Rojas <52978959+MaicolRojas@users.noreply.github.com>
2021-12-07 21:34:42 -0500

    sprint5

---
+++ app/src/main/java/com/example/systeminventory/model/Ubication.kt
@@ -0,0 +1,8 @@
+package com.example.systeminventory.model
+
+class Ubication {
+    val name = "System Inventory"
+    val address= "Villavicencio, Meta"
+    var latitude= 4.1167063
+    var longitude= -73.648947
+}
